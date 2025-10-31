15262b4e835c50cc9799272ba25ed6211f8c7500
Abdul basit <hanifasma90@gmail.com>
2025-10-31 02:54:25 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+alert('you are under 18')
+alert('you are male or female')
+alert('used secure dns')
+alert('ready to open this website')
+alert('please full fill requirements')
+console.log('please enter valid pasward')
+console.log('please enter valid number')
+console.log('please enter valid address')
+console.log('please enter valid post code')
+console.log('thanks')
+confirm('you are under 18')
+confirm('are you male')
+confirm('confirm to verify')
+confirm('are you ready to open this website')
+confirm('ready')
+
+
+
